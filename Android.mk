38d9d951d7780b730b50855bd860783b02ef0767
Kivik <ausa1156red@gmail.com>
2023-03-19 19:41:11 +0300

    initial

---
+++ Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),AGo-32)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_AGo-32.mk
+COMMON_LUNCH_CHOICES := \
+    twrp_AGo-32-eng \
+    twrp_AGo-32-userdebug
