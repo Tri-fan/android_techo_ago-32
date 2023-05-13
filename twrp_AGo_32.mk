25d76e70d4e2f7ff21a04f038b4cd0f4ece64edf
Tri-fan <66644824+Tri-fan@users.noreply.github.com>
2023-05-13 23:55:37 +0300

    rename PRODUCT_NAME and etc

--- twrp_AGo_32.mk
+++ twrp_AGo_32.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-PRODUCT_RELEASE_NAME := AGo_32
+PRODUCT_RELEASE_NAME := ago_32
 
-PRODUCT_NAME := twrp_AGo_32
-PRODUCT_DEVICE := AGo_32
+PRODUCT_NAME := twrp_ago_32
+PRODUCT_DEVICE := ago_32
 PRODUCT_BRAND := Tecno
 PRODUCT_MODEL := Tecno Pop 6 Pro
 PRODUCT_MANUFACTURER := TECNO
