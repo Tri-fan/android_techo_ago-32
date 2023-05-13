a353b2fe04e53b6b4133973075d78bbb203eb71c
Tri-fan <66644824+Tri-fan@users.noreply.github.com>
2023-05-13 23:56:23 +0300

    rename

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_AGo_32.mk
+    $(LOCAL_DIR)/twrp_ago_32.mk
 COMMON_LUNCH_CHOICES := \
-    twrp_AGo_32-eng \
-    twrp_AGo_32-userdebug
+    twrp_ago_32-eng \
+    twrp_ago_32-userdebug
